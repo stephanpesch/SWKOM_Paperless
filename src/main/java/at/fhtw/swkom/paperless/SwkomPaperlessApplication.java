d3f0e37b15095a19bc5d10a1486c863408198563
stephanpesch <stephan.pesch1@gmail.com>
2023-09-13 17:19:33 +0200

    Initial commit

---
+++ src/main/java/at/fhtw/swkom/paperless/SwkomPaperlessApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.swkom.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwkomPaperlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwkomPaperlessApplication.class, args);
+	}
+
+}
