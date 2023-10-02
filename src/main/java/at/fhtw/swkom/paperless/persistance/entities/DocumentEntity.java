f40c042c584d5c14e32fc5eb492f899082242cb7
stephanpesch <stephan.pesch1@gmail.com>
2023-10-02 14:28:04 +0200

    Added DocumentEntity

---
+++ src/main/java/at/fhtw/swkom/paperless/persistance/entities/DocumentEntity.java
@@ -0,0 +1,43 @@
+package at.fhtw.swkom.paperless.persistance.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class DocumentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Integer correspondent;
+    private Integer documentType;
+    private Integer storagePath;
+    private String title;
+    private String content;
+    @ElementCollection
+    @CollectionTable(name = "tags")
+    @Column(name = "tags")
+    private List<String> tags = new ArrayList<>();
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime created;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime createdDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime modified;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime added;
+    private String archiveSerialNumber;
+    private String originalFileName;
+    private String archivedFileName;
+}
