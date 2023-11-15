eefbc8d435a54c3955ea6719df9d164f8a8c393b
LucatChen <xl.chen@protonmail.com>
2023-11-15 18:22:08 +0100

    DocumentMapper finished!!

--- src/main/java/at/fhtw/swkom/paperless/services/mapper/DocumentMapper.java
+++ src/main/java/at/fhtw/swkom/paperless/services/mapper/DocumentMapper.java
@@ -4,19 +4,72 @@ import at.fhtw.swkom.paperless.persistance.entities.DocumentEntity;
 import at.fhtw.swkom.paperless.services.dto.Document;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import java.util.Collections;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DocumentMapper {
     DocumentMapper INSTANCE = Mappers.getMapper(DocumentMapper.class);
 
-    //@Mapping(target = "correspondent", expression = "java(document.getCorrespondent().orElse(null))")
-    //@Mapping(target = "documentType", expression = "java(document.getDocumentType().orElse(null))")
-    //@Mapping(target = "storagePath", expression = "java(document.getStoragePath().orElse(null))")
-    //@Mapping(target = "tags", expression = "java(document.getTags().orElse(null))")
+
         // Add mappings for other JsonNullable fields
+        @Mapping(target = "correspondent", source = "correspondent", qualifiedByName = "unwrapInteger")
+        @Mapping(target = "documentType", source = "documentType", qualifiedByName = "unwrapInteger")
+        @Mapping(target = "storagePath", source = "storagePath", qualifiedByName = "unwrapInteger")
+        @Mapping(target = "title", source = "title", qualifiedByName = "unwrapString")
+        @Mapping(target = "content", source = "content", qualifiedByName = "unwrapString")
+        @Mapping(target = "tags", source = "tags", qualifiedByName = "unwrapListInteger")
+        @Mapping(target = "archiveSerialNumber", source = "archiveSerialNumber", qualifiedByName = "unwrapString")
+        @Mapping(target = "originalFileName", source = "originalFileName", qualifiedByName = "unwrapString")
+        @Mapping(target = "archivedFileName", source = "archivedFileName", qualifiedByName = "unwrapString")
     DocumentEntity toDocumentEntity(Document document);
 
     // Inverse mapping
-    Document fromDocumentEntity(DocumentEntity documentEntity);
+    @Mapping(target = "correspondent", source = "correspondent", qualifiedByName = "wrapInteger")
+    @Mapping(target = "documentType", source = "documentType", qualifiedByName = "wrapInteger")
+    @Mapping(target = "storagePath", source = "storagePath", qualifiedByName = "wrapInteger")
+    @Mapping(target = "title", source = "title", qualifiedByName = "wrapString")
+    @Mapping(target = "content", source = "content", qualifiedByName = "wrapString")
+    @Mapping(target = "tags", source = "tags", qualifiedByName = "wrapListInteger")
+    @Mapping(target = "archiveSerialNumber", source = "archiveSerialNumber", qualifiedByName = "wrapString")
+    @Mapping(target = "originalFileName", source = "originalFileName", qualifiedByName = "wrapString")
+    @Mapping(target = "archivedFileName", source = "archivedFileName", qualifiedByName = "wrapString")
+    Document toDocumentDTO(DocumentEntity documentEntity);
+
+    // Custom methods to handle JsonNullable unwrapping
+    @Named("unwrapInteger")
+    default Integer unwrapInteger(JsonNullable<Integer> jsonNullable) {
+        return jsonNullable.orElse(null);
+    }
+
+    @Named("unwrapString")
+    default String unwrapString(JsonNullable<String> jsonNullable) {
+        return jsonNullable.orElse(null);
+    }
+
+    @Named("unwrapListInteger")
+    default List<Integer> unwrapListInteger(JsonNullable<List<Integer>> jsonNullable) {
+        return jsonNullable.orElse(Collections.emptyList());
+    }
+
+
+    @Named("wrapInteger")
+    default JsonNullable<Integer> wrapInteger(Integer value) {
+        return JsonNullable.of(value);
+    }
+
+    @Named("wrapString")
+    default JsonNullable<String> wrapString(String value) {
+        return JsonNullable.of(value);
+    }
+
+    @Named("wrapListInteger")
+    default JsonNullable<List<Integer>> wrapListInteger(List<Integer> value) {
+        return JsonNullable.of(value);
+    }
+
 }
